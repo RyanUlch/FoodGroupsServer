60a6c430d3292d9c433f7123094165ddd13b319d
Ryan Ulch <RyanUlchDev@Gmail.com>
2022-09-13 15:58:02 -0400

    Added Helmet and Node Version to package.json

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path 			= require('path');
 const cookieParser 	= require('cookie-parser');
 const logger 		= require('morgan');
 const cors 			= require('cors');
+const helmet		= require("helmet");
 
 // Route Imports
 const accountsRouter 	= require('./routes/accounts');
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 // Route connections with app
 app.use('/groups', groupsRouter);
